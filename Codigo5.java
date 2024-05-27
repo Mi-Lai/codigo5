8a61bc13cf2a79395b9402fda36b6f5caacf9335
Mi-Lai Yamamoto Joo <92226500+Mi-Lai@users.noreply.github.com>
2024-05-27 12:34:02 -0600

    codigo otros 5

---
+++ Codigo5.java
@@ -0,0 +1,35 @@
+package codigo;
+import java.util.Scanner;
+
+
+
+public class Codigo5 {
+public static void main (String[] args) {
+    Scanner s = new Scanner(System.in);
+    System.out.println("Introduzca un número: ");
+    int ni;
+     ni = s.nextInt();
+    int c = ni;
+    
+    int afo = 0;
+    int noAfo = 0;
+    
+    while (ni > 0) {
+	  int digito = (int)(ni % 10);
+      if ((digito == 3) || (digito == 7) || (digito == 8) || (digito == 9)) {
+		afo++;
+      } else {
+		noAfo++;
+	  
+	  ni /= 10;
+    }
+    if (afo > noAfo) {
+      System.out.println("El " + c + " es un número afortunado.");
+    } else {
+      System.out.println("El " + c+ " no es un número afortunado.");
+    }
+    
+  }
+  
+}
+}
